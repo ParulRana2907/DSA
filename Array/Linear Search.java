9af5dc548b7a5deda9b02f1d1ca25ae3236d40a2
Parul Rana <parulrana2407@gmail.com>
2026-06-06 22:17:20 +0530

    Update Linear Search.java

--- Array/Linear Search.java
+++ Array/Linear Search.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class LinearSearch {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24CSU292");
+        int[] arr = {10, 25, 30, 45, 50};
+        int key;
+        boolean found = false;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter element to search: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
