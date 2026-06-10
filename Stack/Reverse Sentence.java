35a196e7d5a7b15687f72b8b9f732302bf3e065e
Parul Rana <parulrana2407@gmail.com>
2026-06-10 21:54:29 +0530

    Reverse Sentence.java

--- Stack/Reverse Sentence.java
+++ Stack/Reverse Sentence.java
@@ -1,1 +1,24 @@
+import java.util.Stack;
 
+public class ReverseSentenceStack {
+    public static void main(String[] args) {
+
+        String str = "data structures and algorithms";
+
+        System.out.println("Original Sentence: " + str);
+
+        Stack<String> stack = new Stack<>();
+
+        String words[] = str.split(" ");
+
+        for (String w : words) {
+            stack.push(w);
+        }
+
+        System.out.print("Reversed Sentence: ");
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
