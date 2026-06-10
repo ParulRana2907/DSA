80221223a307c90108e694a523a4a3e16b1369e3
Parul Rana <parulrana2407@gmail.com>
2026-06-10 21:52:09 +0530

    Stack Linked List.java

--- Stack/Stack Linked List.java
+++ Stack/Stack Linked List.java
@@ -1,1 +1,62 @@
+class Node {
+    int data;
+    Node next;
 
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+}
+
+class StackLinkedList {
+    Node top;
+
+    // PUSH
+    void push(int val) {
+        Node newNode = new Node(val);
+        newNode.next = top;
+        top = newNode;
+        System.out.println(val + " pushed");
+    }
+
+    // POP
+    void pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+        } else {
+            System.out.println(top.data + " popped");
+            top = top.next;
+        }
+    }
+
+    // PEEK
+    void peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println("Top element is: " + top.data);
+        }
+    }
+
+    // ISEMPTY
+    boolean isEmpty() {
+        return top == null;
+    }
+
+    // ISFULL
+    boolean isFull() {
+        return false; // Linked list stack is never full unless memory ends
+    }
+
+    public static void main(String[] args) {
+        StackLinkedList s = new StackLinkedList();
+
+        s.push(5);
+        s.push(15);
+        s.push(25);
+
+        s.peek();
+        s.pop();
+        s.peek();
+    }
+}
