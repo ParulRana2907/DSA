ae233a7b4224f0517f8b21783652ae23af975fb2
Parul Rana <parulrana2407@gmail.com>
2026-06-20 22:34:04 +0530

    Smallest Element.java

--- Array/Smallest Element.java
+++ Array/Smallest Element.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+// Minimum/Smallest element inside an array to be checked
+
+public class Main {
+	static int SmallestElement(int[] arr){
+		int max = Integer.MAX_VALUE;
+		for (int i=0 ; i<arr.length ; i++) {
+			if (arr[i]< max) {
+				max = arr[i];
+			}
+		}
+		return max;
+		
+	}	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the length of the Array:");
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the Elements of the Array:");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		int res = SmallestElement(arr);
+		System.out.println("The Smallest element in the array is:");
+		System.out.println(res);
+	}
+}
