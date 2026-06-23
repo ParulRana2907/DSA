22f27a39ce48753f5db454c3986128485020a086
Parul Rana <parulrana2407@gmail.com>
2026-06-23 20:47:17 +0530

    Design circular queue.java

--- Queue/Design circular queue.java
+++ Queue/Design circular queue.java
@@ -1,1 +1,57 @@
+import java.util.*
+class MyCircularQueue {
+        int [] arr;
+        int front = -1;
+        int rear = -1;
+        int size;
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        size = k;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull()) return false;
+        if(isEmpty()) 
+        {
+            front = 0;
+            rear = 0;
+        }
+        else {
+            rear = (rear+1)%size;
+        }
+        arr[rear] = value;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty()) return false;
+        if(front == rear ) {
+        front =-1;
+        rear=-1;
+        }
+        else front = (front +1)%size;
+        return true;
+    }
+    
+    public int Front() {
+        if(isEmpty())  return -1;
+        
+        return arr[front];
+    }
+    
+    public int Rear() {
+        if(isEmpty()) return -1;
+
+        return arr[rear];
+    }
+    
+    public boolean isEmpty() {
+     return front == -1 &&rear==-1;
+
+    }
+    
+    public boolean isFull() {
+     return (rear+1)%size == front ;   
+    }
+}
 
