206c52127132cd286966636e33d8239189fe07cc
Parul Rana <parulrana2407@gmail.com>
2026-06-23 20:43:43 +0530

    Implement queue using stack.java

--- Queue/Implement queue using stack.java
+++ Queue/Implement queue using stack.java
@@ -1,1 +1,23 @@
+import.java.util.*
+class MyQueue {
+    int[] arr = new int[1000];
+    int front = 0;
+    int rear = 0;
+
+    public void push(int x) {
+        arr[rear++] = x;
+    }
+
+    public int pop() {
+        return arr[front++];
+    }
+
+    public int peek() {
+        return arr[front];
+    }
+
+    public boolean empty() {
+        return front == rear;
+    }
+}
 
