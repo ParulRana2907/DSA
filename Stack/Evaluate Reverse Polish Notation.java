8f4004ce289fb6bc07a1fa538341cf4a05b367c2
Parul Rana <parulrana2407@gmail.com>
2026-07-01 21:36:29 +0530

    Evaluate Reverse Polish Notation.java

--- Stack/Evaluate Reverse Polish Notation.java
+++ Stack/Evaluate Reverse Polish Notation.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+class Solution {
+    public int evalRPN(String[] t) {
+        Stack<Integer> s = new Stack<>();
+        
+        for (String x : t) {
+            switch (x) {
+                case "+": s.push(s.pop() + s.pop()); break;
+                case "-": { int b = s.pop(), a = s.pop(); s.push(a - b); break; }
+                case "*": s.push(s.pop() * s.pop()); break;
+                case "/": { int b = s.pop(), a = s.pop(); s.push(a / b); break; }
+                default: s.push(Integer.parseInt(x));
+            }
+        }
+        return s.pop();
+    }
+}
