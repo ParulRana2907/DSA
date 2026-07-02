d132eb317bc758f9d7a728c7cd7ec757b0007c4b
Parul Rana <parulrana2407@gmail.com>
2026-07-02 22:23:14 +0530

    First Unique Character in a String.java

--- Queue/First Unique Character in a String.java
+++ Queue/First Unique Character in a String.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        Queue<Integer> q = new LinkedList<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!m.containsKey(ch)) {
+                q.add(i);
+            }
+
+            m.put(ch, m.getOrDefault(ch, 0) + 1);
+
+            while (!q.isEmpty() && m.get(s.charAt(q.peek())) > 1) {
+                q.remove();
+            }
+        }
+
+        return q.isEmpty() ? -1 : q.peek();
+    }
+}
