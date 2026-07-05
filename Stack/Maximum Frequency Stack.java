8c72216518a2a7a2e9f0d977a316b76843a427aa
Parul Rana <parulrana2407@gmail.com>
2026-07-05 22:03:06 +0530

    Maximum Frequency Stack.java

--- Stack/Maximum Frequency Stack.java
+++ Stack/Maximum Frequency Stack.java
@@ -1,1 +1,23 @@
+class FreqStack {
+    Map<Integer, Integer> freq = new HashMap<>();
+    Map<Integer, Stack<Integer>> group = new HashMap<>();
+    int max = 0;
 
+    public FreqStack() {}
+
+    public void push(int val) {
+        int f = freq.getOrDefault(val, 0) + 1;
+        freq.put(val, f);
+
+        max = Math.max(max, f);
+        group.computeIfAbsent(f, k -> new Stack<>()).push(val);
+    }
+
+    public int pop() {
+        int val = group.get(max).pop();
+        freq.put(val, freq.get(val) - 1);
+
+        if (group.get(max).isEmpty()) max--;
+        return val;
+    }
+} 
