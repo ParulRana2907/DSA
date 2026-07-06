9b3f18354f0906c2d202221d48693afa254c4650
Parul Rana <parulrana2407@gmail.com>
2026-07-06 22:19:24 +0530

    Simplify Path.java

--- Stack/Simplify Path.java
+++ Stack/Simplify Path.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        
+        for (String s : path.split("/"))
+            if (s.equals("..")) { if (!st.isEmpty()) st.pop(); }
+            else if (!s.equals("") && !s.equals(".")) st.push(s);
+        
+        return "/" + String.join("/", st);
+    }
+}
